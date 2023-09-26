5874017103dd39c3ee19b7ff92114073571fc26e
Ivan (Vanopr) <mp3mp61@gmail.com>
2023-09-26 13:16:36 +0300

    Add Extensions, SPM PodcastKit, MainScreen ...

--- PodcastApp/Controllers/MainController/Controller/MainViewController.swift
+++ PodcastApp/Controllers/MainController/Controller/MainViewController.swift
@@ -5,12 +5,27 @@
 //  Created by Sergey on 25.09.2023.
 //
 
-class MainViewController: CustomViewController<MainView> {
-    
+import UIKit
+
+class MainViewController: UIViewController {
+    weak var mainCollectionView: MainCollection?
+    var tabBar = UITabBar()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .white
+        setupMainCollectionView()
     }
     
+    private func setupMainCollectionView() {
+        let mainCollectionView = MainCollection()
+        view.addSubview(mainCollectionView)
+        mainCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.leading.trailing.equalTo(view)
+            make.bottom.equalTo(view).offset(tabBar.frame.height)
+            
+        }
+    }
 }
+
