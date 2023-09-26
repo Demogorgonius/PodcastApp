c621f1dd18c13d156a8dffd27a77e0e65d5d4c03
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-09-26 22:06:12 +0500

    Created Show Indicators for Login and Password TextFields

--- PodcastApp/Controllers/AuthScreen/Controller/LoginViewController.swift
+++ PodcastApp/Controllers/AuthScreen/Controller/LoginViewController.swift
@@ -10,25 +10,40 @@ import SnapKit
 import FirebaseAuth
 
 class LoginInViewController: UIViewController {
-
+    
     // MARK: - UI
-
+    
     private let loginLabel: UILabel = {
         return UILabel(labelText: "Логин", textColor: .gray)
     }()
-
+    
     private let loginTextField: UITextField = {
-        return UITextField(placeholder: "Логин", borderStyle: .roundedRect)
+        let textField = UITextField(placeholder: "Логин", borderStyle: .roundedRect)
+        return textField
+    }()
+    
+    private let loginShowInidcator: UIImageView = {
+        let indicator = UIImageView()
+        indicator.image = UIImage(systemName: "eye")
+        indicator.tintColor = .gray
+        return indicator
     }()
     
     private let passwordLabel: UILabel = {
         return UILabel(labelText: "Пароль", textColor: .gray)
     }()
-
+    
     private let passwordTextField: UITextField = {
         return UITextField(placeholder: "Пароль", borderStyle: .roundedRect)
     }()
-
+    
+    private let passwordShowIndicator: UIImageView = {
+        let passwordIndicator = UIImageView()
+        passwordIndicator.image = UIImage(systemName: "eye")
+        passwordIndicator.tintColor = .gray
+        return passwordIndicator
+    }()
+    
     private lazy var loginAndPasswordStack: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [loginLabel, loginTextField, passwordLabel, passwordTextField])
         stackView.axis = .vertical
@@ -36,38 +51,40 @@ class LoginInViewController: UIViewController {
         stackView.distribution = .fill
         return stackView
     }()
-
+    
     private let enterButton: UIButton = {
         let button = UIButton(normalStateText: "Войти", backgroundColor: .blue)
         button.layer.cornerRadius = 25.0
         button.addTarget(enterButtonPressed.self, action: #selector(enterButtonPressed), for: .touchUpInside)
         return button
     }()
-
+    
     // MARK: - Override Functions
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setUpView()
     }
-
+    
 }
 
 extension LoginInViewController {
-
+    
     // MARK: - Private Functions
-
+    
     private func setUpView() {
         addSubviews()
         setConstraints()
     }
-
+    
     private func addSubviews() {
         view.addSubview(loginAndPasswordStack)
+        loginTextField.addSubview(loginShowInidcator)
+        passwordTextField.addSubview(passwordShowIndicator)
         view.addSubview(enterButton)
     }
-
+    
     private func setConstraints() {
         
         loginAndPasswordStack.snp.makeConstraints { make in
@@ -80,16 +97,32 @@ extension LoginInViewController {
             make.height.equalTo(45)
         }
         
+        loginShowInidcator.snp.makeConstraints { make in
+            make.width.equalTo(25)
+            make.height.equalTo(20)
+            make.trailing.equalTo(loginTextField).offset(-10)
+            make.centerY.equalTo(loginTextField)
+        }
+                
         passwordTextField.snp.makeConstraints { make in
             make.height.equalTo(45)
         }
-
+        
+        passwordShowIndicator.snp.makeConstraints { make in
+            make.width.equalTo(25)
+            make.height.equalTo(20)
+            make.trailing.equalTo(passwordTextField).offset(-10)
+            make.centerY.equalTo(passwordTextField)
+        }
+        
         enterButton.snp.makeConstraints { make in
             make.top.equalTo(loginAndPasswordStack.snp.bottom).offset(20)
             make.leading.equalToSuperview().offset(16)
             make.trailing.equalToSuperview().offset(-16)
             make.height.equalTo(57)
         }
+        
+        
     }
     
     
