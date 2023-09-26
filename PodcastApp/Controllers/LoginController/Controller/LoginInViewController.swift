c6c657a7322fb74386a965a83552895a419142e3
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-09-26 10:13:10 +0500

    Created Login and Password Textfields

---
+++ PodcastApp/Controllers/LoginController/Controller/LoginInViewController.swift
@@ -0,0 +1,51 @@
+//
+//  LoginInViewController.swift
+//  PodcastApp
+//
+//  Created by Ислам Пулатов on 9/26/23.
+//
+
+import UIKit
+import SnapKit
+import FirebaseAuth
+
+class LoginInViewController: UIViewController {
+
+    //    MARK: - UI
+    
+    private let loginTextField: UITextField = {
+        return UITextField(placeholder: "Логин", borderStyle: .roundedRect)
+    }()
+    
+    private let password: UITextField = {
+        return UITextField(placeholder: "Пароль", borderStyle: .roundedRect)
+    }()
+    
+    //    MARK: - Override Functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpView()
+    }
+    
+
+}
+
+extension LoginInViewController {
+    
+    //  MARK: - Private Functions
+    
+    private func setUpView() {
+        addSubView()
+        setConstrains()
+    }
+    
+    private func addSubView() {
+        
+    }
+    
+    private func setConstrains() {
+        
+    }
+    
+}
