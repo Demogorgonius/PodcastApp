84fa49a37c828ffe2157468cff77c9f9060f3d49
Sbat2000 <zevzok@gmail.com>
2023-09-27 18:41:20 +0800

    add onboadrdingViewController and onboardingView

---
+++ PodcastApp/Controllers/Onboarding/Controller/OnboardingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  OnboardingViewController.swift
+//  PodcastApp
+//
+//  Created by Aleksandr Garipov on 27.09.2023.
+//
+
+class OnboardingViewController: CustomViewController<OnboardingView> {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
