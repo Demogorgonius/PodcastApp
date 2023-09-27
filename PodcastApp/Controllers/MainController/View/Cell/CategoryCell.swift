10099636c39ddcec55439109045aa19c0a923d29
Demogorgonius <51359795+Demogorgonius@users.noreply.github.com>
2023-09-27 20:31:51 +0300

    Add controllers

--- PodcastApp/Controllers/MainController/View/Cell/CategoryCell.swift
+++ PodcastApp/Controllers/MainController/View/Cell/CategoryCell.swift
@@ -44,7 +44,7 @@ class CategoryCell: UICollectionViewCell {
   private func setupViews() {
       stackView.addArrangedSubview(categoryLabelTob)
       stackView.addArrangedSubview(categoryLabelBottom)
-    contentView.addSubview(stackView)
+      contentView.addSubview(stackView)
   }
 
   private func setupConstraints() {
