ca8ae9afdfc903a9fa37774d0b3029f3843e87d1
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-09-30 11:27:57 +0500

    Created All UiI For CreaateAccountViewController

--- PodcastApp/Controllers/CreateAccountController/View/CreateAccountView.swift
+++ PodcastApp/Controllers/CreateAccountController/View/CreateAccountView.swift
@@ -35,6 +35,62 @@ class CreateAccountView: UIView {
         return button
     }()
     
+    private lazy var leftStrightLine: LineView = {
+        let lineView = LineView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        lineView.backgroundColor = .clear
+        return lineView
+    }()
+    
+    private lazy var continueWithLabel: UILabel = {
+        let label = UILabel(labelText: "Or continue with", textColor: .gray)
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+
+    private lazy var rightStraightLine: LineView = {
+        let lineView = LineView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        lineView.backgroundColor = .clear
+        return lineView
+    }()
+    
+    private lazy var continueStack: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [leftStrightLine, continueWithLabel, rightStraightLine])
+        stackView.axis = .horizontal
+        stackView.spacing = 0
+        stackView.distribution = .fill
+        return stackView
+    }()
+    
+    private let continueWithGoogleLabel: UIButton = {
+        var buttonConfiguration = UIButton.Configuration.plain()
+        buttonConfiguration.title = "Continue with Google"
+        buttonConfiguration.image = UIImage(named: "googleSymbol")
+        buttonConfiguration.imagePadding = 12
+        buttonConfiguration.baseForegroundColor = .black
+        let button = UIButton(configuration: buttonConfiguration)
+        button.backgroundColor = .clear
+        button.layer.cornerRadius = 20
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderWidth = 1.0
+//        button.addTarget(continueWithGoogleButtonPressed.self, action: #selector(continueWithGoogleButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private let registerLabel: UILabel = {
+        let label = UILabel()
+        let text = "Already have an account? Login"
+        let attributedText = NSMutableAttributedString(string: text)
+        attributedText.addAttribute(.foregroundColor, value: UIColor.gray, range: NSRange(location: 0, length: text.count))
+        let purpleRange = (text as NSString).range(of: "Login")
+        attributedText.addAttribute(.foregroundColor, value: UIColor.lightPurple, range: purpleRange)
+        label.attributedText = attributedText
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.isUserInteractionEnabled = true
+        return label
+    }()
+    
     // MARK: - Initialization
     
     override init(frame: CGRect) {
@@ -58,6 +114,9 @@ class CreateAccountView: UIView {
         addSubview(emailLabel)
         addSubview(emailTextField)
         addSubview(continueButton)
+        addSubview(continueStack)
+        addSubview(continueWithGoogleLabel)
+        addSubview(registerLabel)
     }
     
     private func setConstrains() {
@@ -78,6 +137,33 @@ class CreateAccountView: UIView {
             make.leading.equalToSuperview().offset(20)
             make.trailing.equalToSuperview().offset(-20)
             make.height.equalTo(56)
+        }
+        
+        leftStrightLine.snp.makeConstraints { make in
+            make.width.equalTo(67)
+        }
+        
+        rightStraightLine.snp.makeConstraints { make in
+            make.width.equalTo(67)
+        }
+        
+        continueStack.snp.makeConstraints { make in
+            make.top.equalTo(continueButton.snp.bottom).offset(32)
+            make.leading.equalToSuperview().offset(57)
+            make.trailing.equalToSuperview().offset(-57)
+            make.height.equalTo(30)
+        }
+        
+        continueWithGoogleLabel.snp.makeConstraints { make in
+            make.top.equalTo(continueStack.snp.bottom).offset(56)
+            make.leading.equalToSuperview().offset(24)
+            make.trailing.equalToSuperview().offset(-24)
+            make.height.equalTo(56)
+        }
+        
+        registerLabel.snp.makeConstraints { make in
+            make.bottom.equalTo(safeAreaLayoutGuide).offset(-33)
+            make.centerX.equalToSuperview()
         }
     }
     
