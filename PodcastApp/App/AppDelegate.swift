31bb0de541046663af013a01f2ae662cd9e7b17d
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-10-01 22:17:29 +0500

    Configure FireBase in project
    Add Validators
    Add Navigation Between Auth Controllers

--- PodcastApp/App/AppDelegate.swift
+++ PodcastApp/App/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import PodcastIndexKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         PodcastIndexKit.setup(apiKey: "D45ZGFQ26LEQC7D4D5PW", apiSecret: "ECjvzUTaqq3QK7LRA^RjWgbmmCfCcK9q#9pKAA53", userAgent: "MyiOSApp/1.0 (iPhone; iOS 15.0; Scale/2.0)")
+        
+        FirebaseApp.configure()
         
         return true
     }
--- PodcastApp/App/SceneDelegate.swift
+++ PodcastApp/App/SceneDelegate.swift
@@ -12,11 +12,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
 
+//    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+//        guard let windowScene = scene as? UIWindowScene else { return }
+//        let window = UIWindow(windowScene: windowScene)
+//        window.makeKeyAndVisible()
+//        window.rootViewController = LoginInViewController()
+//        self.window = window
+//    }
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
         let window = UIWindow(windowScene: windowScene)
         window.makeKeyAndVisible()
-        window.rootViewController = CustomTabBarController()
+        
+        // Create a navigation controller and set your CreateAccountViewController as its root view controller
+        let createAccountViewController = LoginInViewController()
+        let navigationController = UINavigationController(rootViewController: createAccountViewController)
+        
+        window.rootViewController = navigationController
         self.window = window
     }
 
