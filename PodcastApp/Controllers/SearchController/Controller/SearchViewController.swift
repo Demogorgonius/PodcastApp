64dc352e19d151ccd0a6b0cec42d6fa5fd2fa739
Ivan (Vanopr) <mp3mp61@gmail.com>
2023-10-03 22:40:02 +0300

    Add Search logic

--- PodcastApp/Controllers/SearchController/Controller/SearchViewController.swift
+++ PodcastApp/Controllers/SearchController/Controller/SearchViewController.swift
@@ -10,19 +10,24 @@ import PodcastIndexKit
 
 class SearchViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     let podcastIndexKit = PodcastIndexKit()
+    let searchCollectionView = SearchCollectionView()
     let searchView = SearchView()
+    let searchResultView = SearchResultView()
     var categoriesArray: CategoriesResponse? {
         didSet {
-            searchView.categoriesCollectionView.reloadData()
+            searchCollectionView.categoriesCollectionView.reloadData()
         }
     }
+    
+    private var podcasts: PodcastArrayResponse?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        searchView.searchBar.delegate = self
+
         fetchData()
-        
-        
+                
         let gradientLayer = CAGradientLayer()
         let colors = [UIColor.redSearch, UIColor.hexadecimal]
            gradientLayer.colors = colors.map { $0.cgColor }
@@ -31,21 +36,48 @@ class SearchViewController: UIViewController, UICollectionViewDataSource, UIColl
         gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
            view.layer.insertSublayer(gradientLayer, at: 0)
         
+        setupSearch()
+
         
+        searchCollectionView.seeAllButton.addTarget(self, action: #selector(seeAllAction), for: .touchUpInside)
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        searchResultView.isHidden = true
+        searchCollectionView.isHidden = false
+        setupMainCollectionView()
+        searchView.searchBar.text = nil
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    private func setupSearch() {
         view.addSubview(searchView)
         searchView.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide)
             make.leading.trailing.equalTo(view)
+            make.height.equalTo(50)
+        }
+    }
+    
+    private func setupMainCollectionView() {
+        view.addSubview(searchCollectionView)
+        setupCollectionViewDelegate(searchCollectionView.genresCollectionView)
+        setupCollectionViewDelegate(searchCollectionView.categoriesCollectionView)
+        searchCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(searchView.snp.bottom)
+            make.leading.trailing.equalTo(view)
             make.bottom.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
-        setupCollectionViewDelegate(searchView.genresCollectionView)
-        setupCollectionViewDelegate(searchView.categoriesCollectionView)
-        
-        searchView.seeAllButton.addTarget(self, action: #selector(seeAllAction), for: .touchUpInside)
     }
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.navigationBar.isHidden = true
+    
+    private func setupSearchResults() {
+        view.addSubview(searchResultView)
+        setupCollectionViewDelegate(searchResultView.collectionView)
+        searchResultView.snp.makeConstraints { make in
+            make.top.equalTo(searchView.snp.bottom).offset(10)
+            make.leading.trailing.equalTo(view)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
     }
     
     @objc func seeAllAction() {
@@ -71,30 +103,83 @@ class SearchViewController: UIViewController, UICollectionViewDataSource, UIColl
         }
     }
     
+    private func fetchDataForSearch(text: String) {
+        Task {
+            do {
+                let data = try await podcastIndexKit.searchService.search(byTerm: text)
+                podcasts = data
+                searchResultView.collectionView.reloadData()
+            } catch {
+                print("Произошла ошибка: \(error)")
+            }
+        }
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if collectionView == searchView.genresCollectionView {
+        if collectionView == searchCollectionView.genresCollectionView {
             return 10
-        } else {
+        } else if collectionView == searchCollectionView.categoriesCollectionView {
             return categoriesArray?.count ?? 2
+        } else {
+            return podcasts?.feeds?.count ?? 0
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchCell.identifier, for: indexPath) as! SearchCell
         
-        if collectionView == searchView.genresCollectionView {
+        if collectionView == searchCollectionView.genresCollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchCell.identifier, for: indexPath) as! SearchCell
             cell.configure(with: TopGenresModel.topGenres[indexPath.row])
-        } else {
+            return cell
+        } else if collectionView == searchCollectionView.categoriesCollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchCell.identifier, for: indexPath) as! SearchCell
             cell.configure(with: categoriesArray?.feeds?[indexPath.row].name ?? "")
+            return cell
+        } else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PodcastCell", for: indexPath) as! PodcastCell
+            let podcast = podcasts?.feeds?[indexPath.row]
+            FetchImage.loadImageFromURL(urlString: podcast?.image ?? "") { image in
+                let resizedImage = FetchImage.resizeImage(image: image, targetSize: CGSize(width: 50, height: 50))
+                DispatchQueue.main.async {
+                    cell.setupPodcastCell(
+                        titleLeft: podcast?.title ?? "",
+                        titleRight: podcast?.author ?? "",
+                        descriptionLeft: podcast?.categories?.values.joined(separator: " & ") ?? "",
+                        descriptionRight: "Right",
+                        image: resizedImage,
+                        cellType: .podcast)
+                    cell.checkmarkButton.isHidden = true
+                }
+            }
+                    return cell
         }
-        return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = (view.frame.width - 64 - 10)/2
-        let height = 0.57 * width
-        return CGSize(width: width, height: height)
+        if collectionView == searchResultView.collectionView {
+            return CGSize(width: collectionView.layer.frame.width, height: 72)
+        } else {
+            let width = (view.frame.width - 64 - 10)/2
+            let height = 0.57 * width
+            return CGSize(width: width, height: height)
+        }
     }
     
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            searchResultView.isHidden = true
+            searchCollectionView.isHidden = false
+            setupMainCollectionView()
+        } else {
+            searchCollectionView.isHidden = true
+            searchResultView.isHidden = false
+            setupSearchResults()
+            fetchDataForSearch(text: searchText)
+        }
+    }
+
 }
--- PodcastApp/Controllers/SearchController/View/SearchView.swift
+++ PodcastApp/Controllers/SearchController/View/SearchView.swift
@@ -1,102 +1,29 @@
 //
-//  SearchView.swift
+//  Search.swift
 //  PodcastApp
 //
-//  Created by Sergey on 25.09.2023.
+//  Created by Vanopr on 03.10.2023.
 //
 
 import Foundation
 import UIKit
-import SnapKit
 
 class SearchView: UIView {
+    let searchBar: UISearchBar = {
+           let searchBar = UISearchBar()
+           searchBar.placeholder = "Podcast, channel, artist"
+           searchBar.backgroundImage = UIImage()
+           searchBar.barTintColor = .white
+           searchBar.layer.cornerRadius = 12
+           searchBar.clipsToBounds = true
+           searchBar.translatesAutoresizingMaskIntoConstraints = false
+           return searchBar
+       }()
     
     override init(frame: CGRect) {
-      super.init(frame: frame)
+        super.init(frame: frame)
         addSubview(searchBar)
-        addSubview(topGenresLabel)
-        addSubview(seeAllButton)
-        addSubview(genresCollectionView)
-        addSubview(browseAll)
-        addSubview(categoriesCollectionView)
-        setupConstrains()
         
-        if let textField = searchBar.value(forKey: "searchField") as? UITextField {
-            textField.backgroundColor = UIColor.white
-            textField.leftView = nil
-            let imageView = UIImageView(image: UIImage(systemName: "magnifyingglass")?.withTintColor(.black))
-            textField.rightView = imageView
-            
-        }
-
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private let searchBar: UISearchBar = {
-            let searchBar = UISearchBar()
-            searchBar.placeholder = "Podcast, channel, artist"
-            searchBar.backgroundImage = UIImage() // Удаляем фоновое изображение
-            searchBar.barTintColor = .white // Задаем цвет фона
-            searchBar.layer.cornerRadius = 12 // Закругляем края
-            searchBar.clipsToBounds = true
-            searchBar.translatesAutoresizingMaskIntoConstraints = false
-
-            return searchBar
-        }()
-    
-    private var topGenresLabel: UILabel = {
-        let label = UILabel()
-        label.font = .manropeBold(size: 16)
-        label.numberOfLines = 1
-        label.text = "Top Genres"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-     let seeAllButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("See All", for: .normal)
-        button.setTitleColor(.gray, for: .normal)
-        button.setImage(UIImage(systemName: "arrow.right"), for: .normal)
-        button.imageView?.tintColor = .gray
-        button.titleLabel?.font = .manropeRegular(size: 16)
-        button.semanticContentAttribute = .forceRightToLeft
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-    
-     let genresCollectionView: UICollectionView = {
-       let layout = UICollectionViewFlowLayout()
-       layout.scrollDirection = .horizontal
-       let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-         collectionView.backgroundColor = .clear
-       collectionView.register(SearchCell.self, forCellWithReuseIdentifier: SearchCell.identifier)
-       return collectionView
-   }()
-    
-    private var browseAll: UILabel = {
-        let label = UILabel()
-        label.font = .manropeBold(size: 16)
-        label.numberOfLines = 1
-        label.text = "Browse All"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-     let categoriesCollectionView: UICollectionView = {
-          let layout = UICollectionViewFlowLayout()
-          layout.scrollDirection = .vertical
-          let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.register(SearchCell.self, forCellWithReuseIdentifier: SearchCell.identifier)
-          collectionView.backgroundColor = .clear
-          collectionView.translatesAutoresizingMaskIntoConstraints = false
-          return collectionView
-      }()
-    
-    private func setupConstrains() {
         searchBar.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(10)
             make.leading.equalToSuperview().offset(32)
@@ -104,40 +31,12 @@ class SearchView: UIView {
             make.height.equalTo(48)
         }
         
-        topGenresLabel.snp.makeConstraints { make in
-            make.top.equalTo(searchBar.snp.bottom).offset(22)
-            make.leading.equalToSuperview().offset(32)
-            make.height.equalTo(22)
+        if let textField = searchBar.value(forKey: "searchField") as? UITextField {
+            textField.backgroundColor = UIColor.white
         }
-        
-        seeAllButton.snp.makeConstraints { make in
-            make.top.equalTo(searchBar.snp.bottom).offset(22)
-            make.trailing.equalToSuperview().inset(32)
-            make.height.equalTo(22)
-        }
-        
-        genresCollectionView.snp.makeConstraints { make in
-            make.top.equalTo(topGenresLabel.snp.bottom).offset(10)
-            make.leading.equalToSuperview().offset(32)
-            make.trailing.equalToSuperview()
-            make.height.equalTo(90)
-        }
-        
-        browseAll.snp.makeConstraints { make in
-            make.top.equalTo(genresCollectionView.snp.bottom).offset(22)
-            make.leading.equalToSuperview().offset(32)
-            make.height.equalTo(22)
-        }
-        
-        categoriesCollectionView.snp.makeConstraints { make in
-            make.top.equalTo(browseAll.snp.bottom).offset(22)
-            make.leading.equalToSuperview().offset(32)
-            make.trailing.equalToSuperview().inset(32)
-            make.bottom.equalToSuperview()
-        }
-        
-        
     }
-
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
