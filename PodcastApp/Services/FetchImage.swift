30977f9c1267ca1db6bc03123f9e3dfc4bbfe471
Ivan (Vanopr) <mp3mp61@gmail.com>
2023-10-05 20:46:10 +0300

    add cash

--- PodcastApp/Services/FetchImage.swift
+++ PodcastApp/Services/FetchImage.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import CommonCrypto
 
 struct FetchImage {
     
@@ -14,18 +15,59 @@ struct FetchImage {
     
     static func loadImageFromURL(urlString: String, completion: @escaping (UIImage?) -> Void) {
         guard let url = URL(string: urlString) else {
-          completion(nil)
-          return
+            completion(nil)
+            return
         }
+        
+        // Вычисляем хеш URL, чтобы использовать его как ключ для кэширования
+        let urlHash = sha256(input: urlString)
+        
+        // Проверяем, есть ли изображение в кэше
+        if let cachedImage = imageCache[urlHash] {
+            completion(cachedImage)
+            return
+        }
+        
         URLSession.shared.dataTask(with: url) { data, response, error in
             guard let data = data, error == nil else {
                 completion(nil)
                 return
             }
+            
             let image = UIImage(data: data)
+            
+            // Если изображение успешно загружено, сохраняем его в кэше
+            if let image = image {
+                imageCache[urlHash] = image
+            }
+            
             completion(image)
         }.resume()
     }
+
+    // Простой кэш для локального хранения изображений
+    private static var imageCache = [String: UIImage]()
+
+    // Функция для вычисления хеша SHA-256 для строки
+    private static func sha256(input: String) -> String {
+        if let data = input.data(using: .utf8) {
+            return sha256(data: data)
+        }
+        return ""
+    }
+
+    // Функция для вычисления хеша SHA-256 для данных
+    private static func sha256(data: Data) -> String {
+        var digest = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
+        
+        data.withUnsafeBytes { (bytes: UnsafeRawBufferPointer) in
+            _ = CC_SHA256(bytes.baseAddress, CC_LONG(data.count), &digest)
+        }
+        
+        let hexBytes = digest.map { String(format: "%02hhx", $0) }
+        return hexBytes.joined()
+    }
+
     
     static func resizeImage(image: UIImage?, targetSize: CGSize) -> UIImage? {
         guard let image = image else {return nil}
