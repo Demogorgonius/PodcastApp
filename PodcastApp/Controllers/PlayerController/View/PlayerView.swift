48ffa10abe01c58ab0ed7ad76aa78771b439eefe
Demogorgonius <51359795+Demogorgonius@users.noreply.github.com>
2023-10-07 08:10:55 +0300

    Add ui elements to player

--- PodcastApp/Controllers/PlayerController/View/PlayerView.swift
+++ PodcastApp/Controllers/PlayerController/View/PlayerView.swift
@@ -20,12 +20,66 @@ class PlayerViewClass: CustomView {
     // MARK: Variables
     weak var delegate: PlayerViewDelegate?
     
-    // MARK: UI
+    // MARK: - --- UI ---
     
+    
+    // MARK: - - Make labels:
+    
+    private lazy var episodeTitle = UILabel.makeLabel(font: .manropeExtraBold(size: 16.0), textColor: .santaGray)
+    private lazy var podcastTitle = UILabel.makeLabel(font: .manropeRegular(size: 16.0), textColor: .purplyGrey)
+    private lazy var timePassedLabel = UILabel.makeLabel(font: .manropeRegular(size: 14.0), textColor: .santaGray)
+    private lazy var timeLeftLabel = UILabel.makeLabel(font: .manropeRegular(size: 14.0), textColor: .santaGray)
+    
+    //MARK: - - Make buttons:
+    
+    private lazy var shuffleButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "shuffle"), for: .normal)
+        button.tintColor = .purplyGrey
+        return button
+    }()
+    
+    private lazy var previousTrackButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "arrowtriangle.left.fill"), for: .normal)
+        button.tintColor = .purplyGrey
+        return button
+    }()
+    
+    private lazy var nextTrackButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "arrowtriangle.right.fill"), for: .normal)
+        button.tintColor = .purplyGrey
+        return button
+    }()
+    
+    private lazy var playTrackButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "play.circle.fill"), for: .normal)
+        button.tintColor = .purplyGrey
+        return button
+    }()
+    
+    private lazy var repeatTrackButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "repeat"), for: .normal)
+        button.tintColor = .purplyGrey
+        return button
+    }()
+    
+    //MARK: - - Make slider:
+    
+    private lazy var progressBar: UISlider = {
+        let slider = UISlider()
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        slider.tintColor = .purplyGrey
+        return slider
+    }()
+
     
     // MARK: setViews
     
-    func setViews() {
+    override func setViews() {
         super.setViews()
         
         
@@ -34,7 +88,7 @@ class PlayerViewClass: CustomView {
     
     // MARK: layoutViews
     
-    func layoutViews() {
+    override func layoutViews() {
         super.layoutViews()
         
         
